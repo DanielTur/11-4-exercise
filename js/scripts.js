96f0ee72fcdc6c1dc01d9621d2c47b9c9570023a
Daniel Turlinski <daniel.turlinski@gmail.com>
2018-01-08 23:49:37 +0100

    Add 11-4-exercise files

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// scripts.js
+
+function Phone(brand, price, color, ram) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.ram = ram;
+}
+
+Phone.prototype.printInfo = function () {
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and price is ' + this.price + '.');
+}
+
+Phone.prototype.printRam = function () {
+	console.log('There is ' + this.ram + ' amount of ram onboard of ' + this.brand + '.');
+}
+
+var iphone6s = new Phone('Apple', 2250, 'silver', '2 GB');
+var samsungGalaxyS6 = new Phone('Samsung', 1999, 'black', '3 GB');
+var onePlusOne = new Phone('OnePlus', 1500, 'white', '3GB');
+
+onePlusOne.printInfo();
+iphone6s.printRam();
